6f5728c39fd4900b52982a33b2b5976c6d87d4b0
Steven Laurino <sjlaurino@gmail.com>
2019-02-06 16:06:10 -0700

    working game w/ random generator

--- app/app.js
+++ app/app.js
@@ -1,13 +1,13 @@
 
 let options = {
   rock: {
-    img('https://i.pinimg.com/originals/cd/63/d9/cd63d97dd4a883f9bba914248a0ff23e.jpg')
+    img: ('https://i.pinimg.com/originals/cd/63/d9/cd63d97dd4a883f9bba914248a0ff23e.jpg')
   },
   paper: {
-    img('https://kopiernet.files.wordpress.com/2016/07/paper.jpg')
+    img: ('https://kopiernet.files.wordpress.com/2016/07/paper.jpg')
   },
   scissors: {
-    img('https://pmcvariety.files.wordpress.com/2015/12/edward-scissorhands.jpg?w=1000&h=563&crop=1')
+    img: ('https://pmcvariety.files.wordpress.com/2015/12/edward-scissorhands.jpg?w=1000&h=563&crop=1')
   }
 }
 
@@ -15,43 +15,44 @@ let options = {
 function random() {
   let choices = Object.keys(options)
   let choiceIndex = Math.floor(Math.random() * choices.length)
-  let choice = options[choiceIndex]
+  let choice = choices[choiceIndex]
   return choice
 }
 
 function play(playerChoice) {
   let cpChoice = random()
+  console.log(cpChoice)
   if (playerChoice == 'rock') {
     if (cpChoice == 'rock') {
-      document.getElementById('result').innerHTML = 'Draw, try again!'
-    } else if (playerChoice == 'paper') {
+      document.getElementById('result').innerHTML = 'DRAW, try again!'
+    } else if (cpChoice == 'paper') {
+      document.getElementById('result').innerHTML = 'YOU\'VE BEEN SMOTHERED! Try again!'
+    } else if (cpChoice == 'scissors') {
       document.getElementById('result').innerHTML = 'YOU WIN!'
-    } else if (playerChoice == 'scissors') {
-      document.getElementById('result').innerHTML = 'You\'ve been crushed! Try again!'
     }
   }
 
   if (playerChoice == 'paper') {
     if (cpChoice == 'paper') {
-      document.getElementById('result').innerHTML = 'Draw, try again!'
-    } else if (playerChoice == 'paper') {
+      document.getElementById('result').innerHTML = 'DRAW, try again!'
+    } else if (cpChoice == 'scissors') {
+      document.getElementById('result').innerHTML = 'YOU\'VE BEEN CUT!, Try again!'
+    } else if (cpChoice == 'rock') {
       document.getElementById('result').innerHTML = 'YOU WIN!'
-    } else if (playerChoice == 'scissors') {
-      document.getElementById('result').innerHTML = 'You\'ve been crushed! Try again!'
     }
   }
 
   if (playerChoice == 'scissors') {
     if (cpChoice == 'scissors') {
-      document.getElementById('result').innerHTML = 'Draw, try again!'
-    } else if (playerChoice == 'paper') {
+      document.getElementById('result').innerHTML = 'DRAW, try again!'
+    } else if (cpChoice == 'paper') {
       document.getElementById('result').innerHTML = 'YOU WIN!'
-    } else if (playerChoice == 'scissors') {
-      document.getElementById('result').innerHTML = 'You\'ve been crushed! Try again!'
+    } else if (cpChoice == 'rock') {
+      document.getElementById('result').innerHTML = 'YOU\'VE BEEN CRUSHED, try again'
     }
   }
 }
 
-random()
+
 
 
