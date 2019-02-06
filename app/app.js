6e635f1adbd245ee7ceabbb03345f1a8d418f818
Steven Laurino <sjlaurino@gmail.com>
2019-02-06 13:31:49 -0700

    working with cp as rock

---
+++ app/app.js
@@ -0,0 +1,37 @@
+
+// let cpChoice = {
+//   rock: {
+//     img('')
+//   },
+
+//   paper: {
+//     img('')
+//   },
+//   scissors: {
+//     img('')
+//   }
+// }
+
+
+
+
+
+function play(playerChoice) {
+  if (playerChoice == 'rock') {
+    document.getElementById('result').innerHTML = 'Draw, try again!'
+  } else if (playerChoice == 'paper') {
+    document.getElementById('result').innerHTML = 'YOU WIN!'
+  } else if (playerChoice == 'scissors') {
+    document.getElementById('result').innerHTML = 'You\'ve been crushed! Try again!'
+  } else {
+    document.getElementById('result').innerHTML = 'Invalid Answer, Try again!'
+  }
+}
+
+// function random(cpChoice) {
+//   let choices = Object.keys(cpChoice)
+//   let choiceIndex = Math.floor(Math.random() * choices.length)
+//   let choice = cpChoice[choiceIndex]
+//   play(choice)
+// }
+
